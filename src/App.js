39fd4c8adc12353b34121e28c1e80d92ec61d13f
unknown <gausulislam5@gmail.com>
2023-10-01 11:39:54 +0600

    react folder structure

--- src/App.js
+++ src/App.js
@@ -25,15 +25,17 @@ function LoadCountries(){
       <h1>Visiting Every Country of the World !!!</h1>
       <h3>Available Countries: {countries.length}</h3>
       {
-        countries.map(country=><p>{country.name.common}</p>)
+        countries.map(country=><Country name={country.name.common} population={country.population}></Country>)
       }
     </div>
   )
 }
-function Country(){
+function Country(props){
   return(
     <div>
-      
+      <h2>Name:{props.name}</h2>
+      <h4>Population:{props.population}</h4>
+
     </div>
   )
 }
