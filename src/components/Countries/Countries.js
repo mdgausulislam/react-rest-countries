e6e011a96fbc2e0fdfe9b633285126cdb074fc86
unknown <gausulislam5@gmail.com>
2023-10-01 20:25:41 +0600

    update grid

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
-    const [countries,setCountries]=useState([]);
-    useEffect(()=>{
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res=>res.json())
-        .then(data=>setCountries(data))
+            .then(res => res.json())
+            .then(data => setCountries(data))
 
-    },[])
+    }, [])
 
     return (
         <div>
             <h1>Hello From Countries.</h1>
             <p>Total Countries: {countries.length}</p>
+            <div className="countries-container">
             {
-                countries.map(country=><Country name={country.name.common} area={country.area} population={country.population}></Country>)
+                countries.map(country => <Country
+                    country={country}>
+                </Country>)
             }
-            
+            </div>
+           
+
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    console.log(props.country);
+    const {name,area,population,region}=props.country;
     return (
         <div className="country">
-            <h2>Country Name:{props.name}</h2>
-            <p>Area: {props.area}</p>
-            <p>population: {props.population} </p>
+            <h2>Country Name:{name.common}</h2>
+            <p>Area: {area}</p>
+            <p>population: {population} </p>
+            <p>Region:{region}</p>
             
         </div>
     );
